945b92b6aa16d732f061edf108f4a3484e4d8af0
pikokr <paring@pikokr.dev>
2022-12-14 01:15:07 +0900

    fix: tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -30,8 +30,28 @@ fn main() {
 
                 if window.is_visible().unwrap() {
                     window.hide().unwrap();
+
+                    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+                    let show = CustomMenuItem::new("show".to_string(), "Show");
+
+                    let tray_menu_show = SystemTrayMenu::new()
+                        .add_item(quit)
+                        .add_native_item(SystemTrayMenuItem::Separator)
+                        .add_item(show);
+
+                    app.tray_handle().set_menu(tray_menu_show).unwrap();
                 } else {
-                    window.hide().unwrap();
+                    window.show().unwrap();
+
+                    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+                    let show = CustomMenuItem::new("hide".to_string(), "Hide");
+
+                    let tray_menu_hide = SystemTrayMenu::new()
+                        .add_item(quit)
+                        .add_native_item(SystemTrayMenuItem::Separator)
+                        .add_item(show);
+
+                    app.tray_handle().set_menu(tray_menu_hide).unwrap();
                 }
             }
             SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
